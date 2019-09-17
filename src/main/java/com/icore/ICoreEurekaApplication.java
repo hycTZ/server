d62f04b3d218c3eef4cf78695097f567f3068870
hycTZ <377957770@qq.com>
2019-09-17 17:20:12 +0800

    TJ

---
+++ src/main/java/com/icore/ICoreEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.icore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class ICoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ICoreEurekaApplication.class, args );
+    }
+}
